da8500e513eb91b46b13612e7f4ca6b7ba6cf773
Kris Szafranski <kdszafranski@gmail.com>
2016-08-15 13:46:29 -0500

    static file server up and running

--- server/app.js
+++ server/app.js
@@ -1,1 +1,15 @@
 // node/express
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var index = require('./routes/index');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Routes
+app.use('/', index);
+
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function() {
+  console.log("Server running on port: ", app.get('port'));
+});
---
+++ server/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+router.get('/*', function(req, res) {
+  // send back static files
+  console.log('file: ', req.params[0]);
+  var file = req.params[0] || '/views/index.html';
+
+  res.sendFile(path.join(__dirname, '../public', file));
+});
+
+module.exports = router;
